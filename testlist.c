288843a283521c94fa831566a7d8adbb37182272
dym816 <74645461+dym816@users.noreply.github.com>
2020-11-26 21:41:54 +0800

    Create testlist.c

---
+++ testlist.c
@@ -0,0 +1,13 @@
+#include <list.c>
+
+int main() {
+	SqList list, *pList=&list;
+	InitList(pList);
+	Insert(pList, 1, 10);
+	Insert(pList, 1, 20);
+	Delete(pList, 2);
+	Insert(pList, 1, 30);
+	Insert(pList, 1, 40);
+	Print(pList);
+	printf("%d",GetData(pList, 2)); 
+}
